27b214f3ded807d25448a49b8cdc69bf212b18d5
Sergi Romeu <sergi.romeu@tiendeo.com>
2020-02-11 12:57:24 +0100

    add getGithubInfo

--- src/services/blGetGithubInfo.js
+++ src/services/blGetGithubInfo.js
@@ -1,3 +1,30 @@
-const blGetGithubInfo = async () => {};
+import request from 'superagent';
+import { githubApiKey, githubApiUrl } from '../config';
+
+const blGetGithubInfo = async () => {
+  const query = ` 
+    query {
+      viewer {
+        pinnedItems(first: 6) {
+          nodes {
+            ... on Repository {
+              id
+              name
+              description
+              url
+            }
+          }
+        }
+      }
+    }`;
+
+  const { body: result } = await request
+    .post(githubApiUrl)
+    .set('Authorization', `bearer ${githubApiKey}`)
+    .set('User-Agent', 'request')
+    .send(JSON.stringify({ query }));
+
+  return result;
+};
 
 export default blGetGithubInfo;
